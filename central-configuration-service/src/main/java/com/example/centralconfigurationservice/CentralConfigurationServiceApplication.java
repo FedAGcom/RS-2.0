8df9feeafef9ffb89ee7d24a80dcc70f7c4873f2
Cherrywoood <cherrywood6918@gmail.com>
2023-02-26 16:23:27 +0300

    feat: add config-server

---
+++ central-configuration-service/src/main/java/com/example/centralconfigurationservice/CentralConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.centralconfigurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralConfigurationServiceApplication.class, args);
+    }
+
+}
